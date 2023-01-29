d342a7c385e835f8f56603677ec13979473269e2
Brendan Hemat-Siraky <brendansirakydeveloper@gmail.com>
2023-01-29 21:56:43 +1000

    wrapped application in setInterval

--- sandbox.js
+++ sandbox.js
@@ -8,39 +8,30 @@ const { getTokenLookup } = require("./src/utils/tokenLookup")
 const tokenLookup = getTokenLookup()
 const startingAmount = 1 // WBNB
 
-getProfitablePath(startingAmount)
-	.then(path => {
-		if (!path) {
-			console.log(`No profitable paths found`)
-		} else {
-			console.log(`Found a profitable trade!`)
-			const jsonToWrite = {
-				...path,
-				gain: `${(((path.finalLiquidity - startingAmount) / startingAmount) * 100).toFixed(6)}%`
-			}
-			fs.writeFileSync(`${__dirname}/logs/${Date()}.json`, JSON.stringify(jsonToWrite, null, 2))
-
-			// const startingAmount = toWei(1, tokenLookup[BASE_TOKEN_ADDRESS].decimals)
-			// executeTriangleSwap(
-			// 	startingAmount,
-			// 	path.tokenPath
-			// )
-		}
-	})
-
 setInterval(() => {
-
+	try {
+		getProfitablePath(startingAmount)
+			.then(path => {
+				if (!path) {
+					console.log(`No profitable paths found`)
+				} else {
+					console.log(`Found a profitable trade!`)
+					const jsonToWrite = {
+						...path,
+						gain: `${(((path.finalLiquidity - startingAmount) / startingAmount) * 100).toFixed(6)}%`
+					}
+					fs.writeFileSync(`${__dirname}/logs/${Date()}.json`, JSON.stringify(jsonToWrite, null, 2))
+		
+					// FOR WHEN WE START ACTUALLY EXECUTING TRADES
+					// const startingAmount = toWei(1, tokenLookup[BASE_TOKEN_ADDRESS].decimals)
+					// executeTriangleSwap(
+					// 	startingAmount,
+					// 	path.tokenPath
+					// )
+				}
+			})
+			// Every 30 seconds
+	} catch (err) {
+		console.error(`Error caught in setInterval`)
+	}
 }, 30000)
-
-// const DAI = '0xDF1742fE5b0bFc12331D8EAec6b478DfDbD31464'
-// const USDC = '0xA2025B15a1757311bfD68cb14eaeFCc237AF5b43'
-// const WBNB = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
-// const BUSD = '0xe9e7cea3dedca5984780bafc599bd69add087d56'
-
-// makeTrade(
-// 	'40000000000000000000', // 10 DAI
-// 	[DAI, USDC, DAI] // DAI -> USDC
-// )
-
-// getReserves()
-// getTradingPairs()
